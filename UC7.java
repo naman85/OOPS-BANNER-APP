2eccc8c4f9821044a1c826647b4d23a290c0f2b6
naman85 <namansharma2745@gmail.com>
2026-03-16 20:38:01 +0530

    uc7

---
+++ UC7.java
@@ -0,0 +1,54 @@
+public class UC7 {
+    public static class CharacterPatternMap {
+        private char character;
+        private String[] pattern;
+
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+        public char getCharacter() {
+            return character;
+        }
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+    public static void main(String[] args) {
+        CharacterPatternMap O = new CharacterPatternMap('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+        CharacterPatternMap P = new CharacterPatternMap('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+        CharacterPatternMap S = new CharacterPatternMap('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        });
+        CharacterPatternMap[] bannerLetters = {O, O, P, S};
+        for (int row = 0; row < 7; row++) {
+            StringBuilder lineBuilder = new StringBuilder();
+            for (CharacterPatternMap letter : bannerLetters) {
+                lineBuilder.append(letter.getPattern()[row]).append("  ");
+            }
+            System.out.println(lineBuilder.toString());
+        }
+    }
+}
